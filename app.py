8a2d94b56ead4559c097d30179ecb7ade54e030f
cedric najdek <cedxx1412@gmail.com>
2024-08-14 22:50:09 +0200

    comparaison with real answer

--- app.py
+++ app.py
@@ -3,21 +3,28 @@ import duckdb
 import streamlit as st
 import ast
 
-# solution_df = duckdb.sql(answer_str).df()
 
 con = duckdb.connect(database="data/exercices_sql_tables.duckdb", read_only=False)
 
 with st.sidebar:
+
     theme = st.selectbox(
         "What would you like to review?",
         ("cross_joins", "GroupBy", "windows_functions"),
         index=None,
         placeholder="Select a theme...",
     )
+
     st.write("You selected:", theme)
 
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
+
+    exercise_name = exercise.loc[0, "exercises_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+solution_df = con.execute(answer).df()
 
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
@@ -26,18 +33,18 @@ if query:
     result = con.execute(query).df()
     st.dataframe(result)
 #
-#     try:
-#         columns = result[solution_df.columns]
-#         result = columns
-#         st.dataframe(result.compare(solution_df))
-#     except KeyError as e:
-#         st.write("Some columns are missing")
-#
-#     n_lines_difference = result.shape[0] - solution_df.shape[0]
-#     if n_lines_difference != 0:
-#         st.write(
-#             f"result has a {n_lines_difference} lines differences with the solution_df"
-#         )
+    try:
+        columns = result[solution_df.columns]
+        result = columns
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"result has a {n_lines_difference} lines differences with the solution_df"
+        )
 #
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 with tab2:
@@ -49,7 +56,6 @@ with tab2:
         st.write(df_table)
 #
 with tab3:
-    exercise_name = exercise.loc[0, "exercises_name"]
-    with open(f"answers/{exercise_name}.sql", "r") as f:
-        answer = f.read()
+
     st.write(answer)
+    st.write(solution_df)
